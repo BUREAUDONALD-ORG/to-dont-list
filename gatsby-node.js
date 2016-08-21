ac01ed9b67b3cb798ca04864844e8163e2c706bc
Zaturrby <rjkorteschiel@gmail.com>
2016-08-21 21:36:19 +0200

    +30 min crossbrowser and autoprefixing

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
 exports.modifyWebpackConfig = function(config, env) {
-  console.log("running loader");
   config.loader('woff', {
     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
     loader: 'url-loader?limit=10000000&minetype=application/font-woff',
@@ -8,5 +9,38 @@ exports.modifyWebpackConfig = function(config, env) {
     test: /\.(eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
     loader: 'url-loader?limit=1000000&minetype=application/vnd.ms-fontobject',
   })
+  switch (env) {
+    case 'develop':
+      config.removeLoader('sass')
+      config.loader('sass', {
+        test: /\.(sass|scss)/,
+        exclude: /\.module\.(sass|scss)$/,
+        loaders: ['style', 'css', 'postcss', 'sass']
+      })
+     config.merge({
+        postcss: [
+          require('postcss-import')(),
+          require('postcss-cssnext')({ browsers: '> 1%' }),
+          require('postcss-browser-reporter'),
+          require('postcss-reporter'),
+        ],
+      })
+      return config
+    case 'build-css':
+      config.loader('sass', {
+        test: /\.(sass|scss)/,
+        exclude: /\.module\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css?minimize', 'postcss', 'sass']),
+      })
+      config.merge({
+        postcss: [
+          require('postcss-import')(),
+          require('postcss-cssnext')({
+            browsers: '> 1%',
+          }),
+        ],
+      })
+      return config
+  }
   return config
 }
