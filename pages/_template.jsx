ebc072b7e9d293269e88f1270563ed75bbb7a3ce
Zaturrby <rjkorteschiel@gmail.com>
2016-10-01 12:11:41 +0200

    +1.30 working on font balancing

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -4,7 +4,7 @@ import { config } from 'config'
 
 import "styles/app.scss";
 import line from '../assets/images/line.png'
-import sidebar from '../content/sidebar.yaml';
+import header from '../content/header.yaml';
 
 module.exports = React.createClass({
   propTypes () {
@@ -17,10 +17,10 @@ module.exports = React.createClass({
       <div className="layout__app">
         <header className="layout__header">
           <div className="header__section">
-            <h1 className="header__title"> {sidebar.siteTitle} </h1>
+            <pre><h1 className="header__title">{header.siteTitle}</h1></pre>
             <nav>
               <ul className="nav__list">
-                {sidebar.nav.map((navItem, key)=>{
+                {header.nav.map((navItem, key)=>{
                   return <li key={key} className="nav__item">{navItem.title}</li>
                 })}
               </ul>
@@ -28,8 +28,8 @@ module.exports = React.createClass({
           </div>
           <img className="header__line" src={line} />
           <div className="header__section">
-            <h3 className="header__subtitle">{sidebar.subTitle}</h3>
-            <h4 className="header__author">{sidebar.authorPrefix} {sidebar.author}</h4>
+            <pre><h3 className="header__subtitle">{header.subTitle}</h3></pre>
+            <h4 className="header__author">{header.authorPrefix} {header.author}</h4>
           </div>
         </header>
         {this.props.children}
--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,15 @@ export default class Index extends React.Component {
     return (
       <DocumentTitle title={config.siteTitle}>
         <main className="layout__main">
-          <div className="layout__delimiter">
-            <div className="tab__container">
-              {[app, book, workshop].map((project, key)=>{
-                return <div key={key} className="tab" onClick={()=>{}}>
-                  <h3 className="tab__title"> { project.title }</h3>
-                  <h2 className="tab__subtitle"> { project.subtitle }</h2>
-                </div>
-              })}
-            </div>
-            <Form />
+          <div className="tab__container">
+            {[app, book, workshop].map((project, key)=>{
+              return <div key={key} className="tab" onClick={()=>{}}>
+                <h3 className="tab__title"> { project.title }</h3>
+                <h2 className="tab__subtitle"> { project.subtitle }</h2>
+              </div>
+            })}
           </div>
+          <Form />
         </main>
       </DocumentTitle>
     )
