f7d95355a131313cb6b4cb0007fa275181a25bad
Zaturrby <rjkorteschiel@gmail.com>
2016-10-01 17:42:12 +0200

    +30min socialbtn

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -5,8 +5,8 @@ import { config } from 'config'
 import "styles/app.scss";
 
 import line from '../assets/images/line.png'
-import header from '../content/header.yaml';
 
+import site from '../content/site.yaml';
 import app from '../content/app.md';
 import book from '../content/book.md';
 import workshop from '../content/workshop.md';
@@ -24,10 +24,10 @@ module.exports = React.createClass({
       <div className="layout__app">
         <header className="layout__header">
           <div className="header__section">
-            <pre><h1 className="header__title">{header.siteTitle}</h1></pre>
+            <pre><h1 className="header__title">{site.siteTitle}</h1></pre>
             <nav>
               <ul className="nav__list">
-                {header.nav.map((navItem, key)=>{
+                {site.nav.map((navItem, key)=>{
                   return <li key={key} className="nav__item">{navItem.title}</li>
                 })}
               </ul>
@@ -35,8 +35,8 @@ module.exports = React.createClass({
           </div>
           <img className="header__line" src={line} />
           <div className="header__section">
-            <pre><h3 className="header__subtitle">{header.subTitle}</h3></pre>
-            <h4 className="header__author">{header.authorPrefix} {header.author}</h4>
+            <pre><h3 className="header__subtitle">{site.subTitle}</h3></pre>
+            <h4 className="header__author">{site.authorPrefix} {site.author}</h4>
           </div>
         </header>
         <main className="layout__main">
@@ -59,6 +59,22 @@ module.exports = React.createClass({
           <Form />
         </div>
         <div className="layout__social">
+          <div className="social__hashtag">
+            {site.hashtag}
+            <span className="social__hashtag-dot"></span>
+          </div>
+          <div className="social__text-container">
+            <p className="social__text">{site.socialText[0]}</p> 
+            <p className="social__text">{site.socialText[1]}</p> 
+          </div>
+          <div className="social__button-container">
+            {site.socialbtn.map((btn, key)=>{
+              return <div key={key} className="social__button">
+                <img className="social__button-img" src={btn.image} />
+                <h1 className="social__button-text">{btn.text}</h1>
+              </div>
+            })}
+          </div>
         </div>
         <div className="layout__credits">
         </div>
