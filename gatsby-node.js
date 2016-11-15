5f8e37fcef1ec8b75022354e08c55059777445f4
Zaturrby <rjkorteschiel@gmail.com>
2016-11-15 17:47:27 +0100

    remove crashing font-loaders

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,14 +2,6 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin'
 let FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 exports.modifyWebpackConfig = function(config, env) {
-  config.loader('woff', {
-    test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    loader: 'url-loader?limit=10000000&minetype=application/font-woff',
-  })
-  config.loader('eot', {
-    test: /\.(eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-    loader: 'url-loader?limit=1000000&minetype=application/vnd.ms-fontobject',
-  })
   switch (env) {
     case 'develop':
       config.removeLoader('sass')
