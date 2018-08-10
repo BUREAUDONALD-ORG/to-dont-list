e731811fd40960c3088b0ee5d21b3555af5c3da8
Zaturrby <rjkorteschiel@gmail.com>
2018-08-10 17:34:06 +0200

    time to read

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ export default class App extends React.Component {
     super(props);
     this.state = { products: {}, timeToRead: 5 };
     this.toggleProducts = this.toggleProducts.bind(this);
+    this.timeToRead = this.timeToRead.bind(this);
   }
 
   toggleProducts(id) {
@@ -20,7 +21,17 @@ export default class App extends React.Component {
     });
   }
 
+  timeToRead() {
+    return this.props.data.products.edges.reduce((acc, product) => {
+      if (this.state.products[product.node.frontmatter.id]) {
+        acc += product.node.frontmatter.timeToRead;
+      }
+      return acc;
+    }, 0);
+  }
+
   render() {
+    console.log(this.timeToRead());
     let products = this.props.data.products.edges;
     let product = products[0].node;
     return (
