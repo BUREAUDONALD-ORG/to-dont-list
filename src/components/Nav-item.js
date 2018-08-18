4c1b5876ee79df1cf5bb0bce7d3baa99e10db359
Zaturrby <rjkorteschiel@gmail.com>
2018-08-18 16:39:41 +0200

    fix animations

--- src/components/Nav-item.js
+++ src/components/Nav-item.js
@@ -22,7 +22,7 @@ export default ({ product, toggleProducts, sticky }) => {
       to={slugify(product.checkbox.title)}
       spy={true}
       smooth={true}
-      offset={-50}
+      offset={-400}
       duration={500}
       isDynamic
     >
