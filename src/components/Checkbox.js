2f3b874fbe3cc78d88bcb77c1f7eadbcd7642d48
Zaturrby <rjkorteschiel@gmail.com>
2018-09-03 23:22:37 +0200

    add disabled animation

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,31 +1,47 @@
 import React from 'react';
 
-export default ({ product, toggleProducts, disabled, ...rest }) => {
-  let animate = e => {
-    console.log(e);
+export default class Checkbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tapped: false
+    };
+  }
+
+  animate = e => {
+    this.setState({ tapped: true }, () => {
+      setTimeout(() => {
+        this.setState({ tapped: false });
+      }, 100);
+    });
   };
-  return (
-    <div
-      className="checkbox"
-      onClick={!disabled ? toggleProducts.bind(this, product) : animate}
-      {...rest}
-    >
-      <input
-        className="checkbox__input"
-        type="checkbox"
-        value={product.checkbox.visible}
-      />
-      <label className="checkbox__label" disabled={disabled}>
-        <div className="checkbox__icon-container">
-          <p className="checkbox__number">
-            {product.checkbox.visible > 0 && product.checkbox.visible}
-          </p>
-        </div>
-        <div className="checkbox__content">
-          <h3 className="checkbox__title">{product.checkbox.title}</h3>
-          <h3 className="checkbox__text">{product.checkbox.text}</h3>
-        </div>
-      </label>
-    </div>
-  );
-};
+
+  render() {
+    let { product, toggleProducts, disabled, ...rest } = this.props;
+    return (
+      <div
+        className="checkbox"
+        data-tapped={this.state.tapped}
+        onClick={!disabled ? toggleProducts.bind(this, product) : this.animate}
+        {...rest}
+      >
+        <input
+          className="checkbox__input"
+          type="checkbox"
+          value={product.checkbox.visible}
+        />
+        <label className="checkbox__label" disabled={disabled}>
+          <div className="checkbox__icon-container">
+            <p className="checkbox__number">
+              {product.checkbox.visible > 0 && product.checkbox.visible}
+            </p>
+          </div>
+          <div className="checkbox__content">
+            <h3 className="checkbox__title">{product.checkbox.title}</h3>
+            <h3 className="checkbox__text">{product.checkbox.text}</h3>
+          </div>
+        </label>
+      </div>
+    );
+  }
+}
