fb5284a697fc2b2115efebab55db8a15d1360c94
Zaturrby <rjkorteschiel@gmail.com>
2018-10-26 08:52:41 +0200

    remove comma

--- src/components/Nav-item.js
+++ src/components/Nav-item.js
@@ -14,7 +14,7 @@ export default ({ product, toggleProducts, sticky }) => {
       to={slugify(product.checkbox.title)}
       spy={true}
       smooth={true}
-      offset={-150}
+      offset={-200}
       duration={500}
       isDynamic
     >
