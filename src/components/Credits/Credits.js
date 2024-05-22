687081f56fc74b429a1e9583ba5a13805b6cef1e
Robert-Jan Korteschiel <rjkorteschiel@gmail.com>
2024-05-22 10:55:06 +0200

    fix window bug and separate out the credits componenet

---
+++ src/components/Credits/Credits.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+
+function Credits() {
+  const credits =
+    useStaticQuery(creditsQuery).credits.edges[0].node.frontmatter;
+
+  return (
+    <div className="layout__credits-container">
+      <div className="layout__credits">
+        <h1 className="credits__title">{credits.title}</h1>
+        <div className="credits__text">
+          <p>
+            {credits.authorsText}
+            {credits.authors.map((author, key) => {
+              if (author.link) {
+                return (
+                  <span key={key}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={author.link}
+                    >
+                      {author.name}
+                    </a>
+                    {", "}
+                  </span>
+                );
+              } else {
+                return (
+                  <span key={key}>
+                    {author.name}
+                    {key === credits.authors.length - 1 ? ", " : ", "}
+                  </span>
+                );
+              }
+            })}
+            {credits.partnersText}
+            {credits.partners.map((partner, key) => {
+              if (partner.link) {
+                return (
+                  <span key={key}>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={partner.link}
+                    >
+                      {partner.name}
+                    </a>
+                    {key === credits.partners.length - 1 ? ". " : ", "}
+                  </span>
+                );
+              } else {
+                return (
+                  <span key={key}>
+                    {partner.name}
+                    {key === credits.partners.length - 1 ? ". " : ", "}
+                  </span>
+                );
+              }
+            })}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const creditsQuery = graphql`
+  query creditsQuery {
+    credits: allMarkdownRemark(
+      filter: {
+        fileAbsolutePath: { regex: "//content/frontpage/site/credits.md/" }
+      }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            authorsText
+            authors {
+              name
+              link
+            }
+            partnersText
+            partners {
+              name
+              link
+            }
+          }
+          html
+        }
+      }
+    }
+  }
+`;
+
+export default Credits;
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ import ProductImage from "../components/Product-image.js";
 
 import Header from "../components/Header/Header.js";
 import ToDontNav from "../components/ToDontNav/ToDontNav.js";
+import Credits from "../components/Credits/Credits.js";
 
 export default class indexPage extends React.Component {
   constructor(props) {
@@ -95,15 +96,20 @@ export default class indexPage extends React.Component {
 
   // this function is used to determine the size of the header
   scrollTriggers = () => {
-    let scrollY = window.scrollY;
-    let navbarElem = document.querySelector(".layout__navbar-container");
-    let navbarPos = navbarElem ? navbarElem.offsetTop : 400;
-    let navbarOffset = navbarPos - 130;
-
     if (typeof window !== "undefined") {
+      let scrollY = window.scrollY;
+      let navbarElem = document.querySelector(".layout__navbar-container");
+      let navbarPos = navbarElem ? navbarElem.offsetTop : 400;
+      let navbarOffset = navbarPos - 130;
+
       return {
-        header: "small",
+        header: this.state.scroll.position < 400 ? "large" : "small",
         nav: scrollY > navbarOffset,
+      };
+    } else {
+      return {
+        header: "large",
+        nav: false,
       };
     }
   };
@@ -132,7 +138,7 @@ export default class indexPage extends React.Component {
     let initialProducts = this.state.initialProducts;
     let form = this.props.data.form.edges[0].node.frontmatter;
     let social = this.props.data.social.edges[0].node.frontmatter;
-    let credits = this.props.data.credits.edges[0].node.frontmatter;
+
     return (
       <Layout>
         <Header />
@@ -343,63 +349,8 @@ export default class indexPage extends React.Component {
               </div>
             </div>
           </div>
-          <div className="layout__credits-container">
-            <div className="layout__credits">
-              <h1 className="credits__title">{credits.title}</h1>
-              <div className="credits__text">
-                <p>
-                  {credits.authorsText}
-                  {credits.authors.map((author, key) => {
-                    if (author.link) {
-                      return (
-                        <span key={key}>
-                          <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href={author.link}
-                          >
-                            {author.name}
-                          </a>
-                          {", "}
-                        </span>
-                      );
-                    } else {
-                      return (
-                        <span key={key}>
-                          {author.name}
-                          {key === credits.authors.length - 1 ? ", " : ", "}
-                        </span>
-                      );
-                    }
-                  })}
-                  {credits.partnersText}
-                  {credits.partners.map((partner, key) => {
-                    if (partner.link) {
-                      return (
-                        <span key={key}>
-                          <a
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            href={partner.link}
-                          >
-                            {partner.name}
-                          </a>
-                          {key === credits.partners.length - 1 ? ". " : ", "}
-                        </span>
-                      );
-                    } else {
-                      return (
-                        <span key={key}>
-                          {partner.name}
-                          {key === credits.partners.length - 1 ? ". " : ", "}
-                        </span>
-                      );
-                    }
-                  })}
-                </p>
-              </div>
-            </div>
-          </div>
+
+          <Credits />
         </div>
         <script
           type="text/javascript"
@@ -525,30 +476,6 @@ export const query = graphql`
               }
             }
           }
-        }
-      }
-    }
-    credits: allMarkdownRemark(
-      filter: {
-        fileAbsolutePath: { regex: "//content/frontpage/site/credits.md/" }
-      }
-    ) {
-      edges {
-        node {
-          frontmatter {
-            title
-            authorsText
-            authors {
-              name
-              link
-            }
-            partnersText
-            partners {
-              name
-              link
-            }
-          }
-          html
         }
       }
     }
