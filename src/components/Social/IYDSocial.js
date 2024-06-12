22364d5de08f88454755a228998e6b8b7847e01a
Robert-Jan Korteschiel <rjkorteschiel@gmail.com>
2024-06-12 13:17:43 +0200

    add line

--- src/components/Social/IYDSocial.js
+++ src/components/Social/IYDSocial.js
@@ -6,30 +6,37 @@ function IYDSocial() {
   let social = useStaticQuery(IYDSocialQuery).social.edges[0].node;
 
   return (
-    <div
-      className="layout__IYDsocial-container"
-      style={{ "--selection-color": "var(--white)" }}
-    >
-      <div className="layout__IYDsocial">
-        <div className="IYDsocial-left">
-          <h1 className="IYDsocial__title">{social.frontmatter.headers[0]}</h1>
-          <div
-            className="markdown"
-            data-font-family="roc-grotesk-compressed"
-            dangerouslySetInnerHTML={{
-              __html: social.html,
-            }}
-          />
-        </div>
+    <>
+      <div
+        className="layout__IYDsocial-container"
+        style={{ "--selection-color": "var(--white)" }}
+      >
+        <div className="layout__IYDsocial">
+          <div className="IYDsocial-left">
+            <h1 className="IYDsocial__title">
+              {social.frontmatter.headers[0]}
+            </h1>
+            <div
+              className="markdown"
+              data-font-family="roc-grotesk-compressed"
+              dangerouslySetInnerHTML={{
+                __html: social.html,
+              }}
+            />
+          </div>
 
-        <div className="IYDsocial-right">
-          <h1 className="IYDsocial__title">{social.frontmatter.headers[1]}</h1>
-          {social.frontmatter.btn.map((btn, key) => {
-            return <Button {...btn} key={key} type="image" />;
-          })}
+          <div className="IYDsocial-right">
+            <h1 className="IYDsocial__title">
+              {social.frontmatter.headers[1]}
+            </h1>
+            {social.frontmatter.btn.map((btn, key) => {
+              return <Button {...btn} key={key} type="image" />;
+            })}
+          </div>
         </div>
       </div>
-    </div>
+      <div className="layout__line-container" />
+    </>
   );
 }
 
